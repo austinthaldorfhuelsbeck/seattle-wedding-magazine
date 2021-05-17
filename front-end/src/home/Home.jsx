49f35b131fb5b6c9f4b100a1783fa30e6b0bcdfc
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-05-16 20:08:33 -0700

    bg img + layout fix

--- front-end/src/home/Home.jsx
+++ front-end/src/home/Home.jsx
@@ -12,18 +12,18 @@ export default function Home() {
             <img
               className="flex-img"
               alt="Seattle Wedding Magazine"
-              src="https://storage.googleapis.com/seattle-wedding-magazine/seattle-wedding-magazine-black%20logo.png" 
+              src="https://storage.googleapis.com/seattle-wedding-magazine/seattle-wedding-magazine-white%20logo.png" 
             />
           </div>
           <div className="row py-5 py-sm-4">
             <div className="col-12 text-center mx-auto">
-              <h1 className="text-9 text-white bg-danger d-inline-block font-weight-700 rounded px-3 py-2 mb-4">
+              <h1 className="text-9 text-white bg-secondary d-inline-block font-weight-700 rounded px-3 py-2 mb-4">
                 <strong>Coming Soon!</strong>
               </h1>
               <h2 className="mb-4">Our new website is on its way.</h2>
             </div>
             <div className="col-md-8 col-lg-6 col-xl-5 mx-auto text-center">
-              <p className="text-muted mb-3">Get notified when we launch.</p>
+              <p className="mb-3">Get notified when we launch.</p>
               <div className="subscribe-form">
                 <form>
                   <div className="input-group">
@@ -39,7 +39,7 @@ export default function Home() {
                       <button
                         id="subscribe-form-submit"
                         name="subscribe-form-email"
-                        className="btn btn-primary px-4 shadow-none"
+                        className="btn btn-secondary px-4 shadow-none"
                         type="submit"
                       >
                         <span className="d-none d-sm-block">Notify Me</span>
@@ -53,9 +53,7 @@ export default function Home() {
         </div>
         <footer className="container text-center">
           <p class="text-muted text-2 mb-2">
-            Copyright &copy; 2021
-            <a href="/">Seattle Wedding Magazine</a>
-            . All Rights Reserved.
+            Copyright &copy; 2021 Seattle Wedding Magazine. All Rights Reserved.
           </p>
         </footer>
       </section>
