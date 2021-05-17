a09f850c3c8b484b8b2558378638ef411d090868
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-05-17 00:34:04 -0700

    fix

--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -5,8 +5,8 @@ export default function NavBar() {
     <nav className="navbar navbar-expand-lg navbar-light">
       <div className="container-fluid">
         <div className="navbar-nav menu-right ml-auto">
-          <Link to="/" className="mx-2 nav-link"><h3><strong>Home</strong></h3></Link>
-          <Link to="/submissions" className="mx-2 nav-link"><h3><strong>Submissions</strong></h3></Link>
+          <Link to="/" className="mx-2 nav-link"><h3>Home</h3></Link>
+          <Link to="/submissions" className="mx-2 nav-link"><h3>Submissions</h3></Link>
         </div>
       </div>
     </nav>
--- front-end/src/home/Home.jsx
+++ front-end/src/home/Home.jsx
@@ -25,7 +25,6 @@ export default function Home() {
           <SubscribeForm />
         </div>
       </div>
-      <
     </Fragment>
   )
 }
